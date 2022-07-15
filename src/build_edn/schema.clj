d1be5afeaa4c9972603789eeb4646dd4c55c478e
liquidz <liquidz.uo@gmail.com>
2022-07-16 08:44:54 +0900

    refactor: Fix schemas to use lite syntax suger

--- src/build_edn/schema.clj
+++ src/build_edn/schema.clj
@@ -1,50 +1,62 @@
-(ns build-edn.schema)
+(ns build-edn.schema
+  (:require
+   [malli.experimental.lite :as l]))
+
+(defn- enum
+  [& xs]
+  (-> (cons :enum xs)
+      (vec)
+      (l/schema)))
+
+(defn- sequential
+  [x]
+  (l/schema [:sequential (l/schema x)]))
 
 (def ?scm
-  [:map
-   [:connection string?]
-   [:developerConnection string?]
-   [:url string?]])
+  (l/schema
+    {:connection string?
+     :developerConnection string?
+     :url string?}))
 
 (def ?build-config
-  [:map
-   [:lib qualified-symbol?]
-   [:version string?]
-   [:description {:optional true} string?]
-   [:source-dir {:optional true} string?]
-   [:class-dir {:optional true} string?]
-   [:jar-file {:optional true} string?]
-   [:scm {:optional true} ?scm]
-   [:github-actions? {:optional true} boolean?]])
+  (l/schema
+   {:lib qualified-symbol?
+    :version string?
+    :description (l/optional string?)
+    :source-dir (l/optional string?)
+    :class-dir (l/optional string?)
+    :jar-file (l/optional string?)
+    :scm (l/optional ?scm)
+    :github-actions? (l/optional boolean?)}))
 
 (def ?uber-build-config
-  [:map
-   [:uber-file string?]
-   [:main symbol?]])
+  (l/schema
+   {:uber-file string?
+    :main symbol?}))
 
 (def ?document
-  [:or
-   [:map
-    [:file string?]
-    [:match string?]
-    [:action [:enum :append-before :replace :append-after]]
-    [:text string?]]
-   [:map
-    [:file string?]
-    [:action [:enum :create]]
-    [:text string?]]])
+  (l/or
+   (l/schema
+    {:file string?
+     :match string?
+     :action (enum :append-before :replace :append-after)
+     :text string?})
+   (l/schema
+    {:file string?
+     :action (enum :create)
+     :text string?})))
 
 (def ?documents-build-config
-  [:map
-   [:documents [:sequential ?document]]])
+  (l/schema
+    {:documents (sequential ?document)}))
 
 (def ?deploy-repository
-  [:map
-   [:id string?]
-   [:username {:optional true} string?]
-   [:password {:optional true} string?]
-   [:url {:optional true} string?]])
+  (l/schema
+   {:id string?
+    :username (l/optional string?)
+    :password (l/optional string?)
+    :url (l/optional string?)}))
 
 (def ?deploy-repository-build-config
-  [:map
-   [:deploy-repository ?deploy-repository]])
+  (l/schema
+   {:deploy-repository ?deploy-repository}))
